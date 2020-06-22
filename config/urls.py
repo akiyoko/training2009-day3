7162b0589ed359d51c47a2ea3e8c3d70f8b0f47d
akiyoko <akiyoko@users.noreply.github.com>
2020-06-22 19:12:12 +0900

    Fix urls.py

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # TODO: 「/」にアクセスした場合に「/todo/」にリダイレクトする
-    # path('', RedirectView.as_view(url='/todo/')),
+    # Note: 「/」にアクセスした場合に「/todo/」にリダイレクトする
+    path('', RedirectView.as_view(url='/todo/')),
     path('todo/', include('todo.urls')),
 ]
